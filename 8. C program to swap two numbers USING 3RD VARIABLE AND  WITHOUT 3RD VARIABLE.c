19373d03218df13082ce09023a299ebe0ac1246c
Prateek Dixit <114977398+Prateek0118@users.noreply.github.com>
2023-01-11 21:37:48 +0530

    Update 8. C program to swap two numbers USING 3RD VARIABLE AND  WITHOUT 3RD VARIABLE.c

--- 8. C program to swap two numbers USING 3RD VARIABLE AND  WITHOUT 3RD VARIABLE.c
+++ 8. C program to swap two numbers USING 3RD VARIABLE AND  WITHOUT 3RD VARIABLE.c
@@ -1,15 +1,23 @@
 #include<stdio.h>  
  int main()    
 {    
-int a, b;
+int a, b,c,d,e;
 printf("Enter the first number:");
 scanf("%d",&a);
 printf("Enter the second number:");
-scanf("%d",&b);      
+scanf("%d",&b);
+  c=a;
+  d=b;
+  printf("without using third variable");
 printf("Before swap a=%d b=%d",a,b);      
 a=a+b;    
 b=a-b;    
 a=a-b;    
-printf("\nAfter swap a=%d b=%d",a,b);    
+printf("\nAfter swap a=%d b=%d",a,b);
+  printf("with using third variable");
+  e=c;
+  c=d;
+  d=e;
+  printf("\nswapped values are %d %d",c,d);_
 return 0;  
 }   
